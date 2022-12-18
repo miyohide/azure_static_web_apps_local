09b85c3c10e575244cf24931da20b0d8b29a94ea
Hidenori Miyoshi <miyohide@gmail.com>
2022-12-18 16:10:13 +0900

    delete unnecessary files

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState, useEffect } from "react";
 
 function App() {
@@ -15,19 +13,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>これはAzure Static Web Appsのサンプルです。</h1>
         <p>APIからの返り値 : <b>{data}</b></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

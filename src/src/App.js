dea4ddec4c3509b12322b700350941fd2f6a0bc2
Hidenori Miyoshi <miyohide@gmail.com>
2022-12-24 15:35:37 +0900

    add import react

--- src/src/App.js
+++ src/src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './NavBar';
 
 function App() {
--- src/src/routes/NoMatch.js
+++ src/src/routes/NoMatch.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function NoMatch() {
   return <h2>このページは存在しません。</h2>;
 }

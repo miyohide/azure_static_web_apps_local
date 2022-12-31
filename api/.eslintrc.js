3d851cfd122918d159967be92af489a01a35f6db
Hidenori Miyoshi <miyohide@gmail.com>
2022-12-31 16:26:13 +0900

    setup extends for eslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "google",
+        "prettier"
+    ],
     "overrides": [
     ],
     "parserOptions": {

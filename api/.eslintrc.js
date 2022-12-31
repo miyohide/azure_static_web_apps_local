c666c1a8b315e31b5a05129152467009b4def63a
Hidenori Miyoshi <miyohide@gmail.com>
2022-12-31 17:14:13 +0900

    run lint-fix command

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": false,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "google",
-        "prettier"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
